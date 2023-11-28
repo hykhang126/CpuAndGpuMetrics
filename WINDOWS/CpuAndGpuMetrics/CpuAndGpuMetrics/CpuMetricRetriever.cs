4bcd9e9b6dbc4f3cbdbf838bbc43156a52260a3a
hykhang126 <hykhang126@gmail.com>
2023-11-28 11:12:19 -0500

    Reorganize repo.
    Add folder for window and another folder for linux

---
+++ WINDOWS/CpuAndGpuMetrics/CpuAndGpuMetrics/CpuMetricRetriever.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using static CpuAndGpuMetrics.CounterReader;
+
+namespace CpuAndGpuMetrics
+{
+    /// <summary>
+    /// Static class to retrieve CPU usage metrics.
+    /// </summary>
+    static internal class CpuMetricRetriever
+    {
+        /// <summary>Time (in ms) before reading CPU usage.</summary>
+        static readonly int TIME = 100;
+
+        /// <summary>
+        /// Gets the current CPU usage and prints it to the console.
+        /// </summary>
+        /// <returns>
+        /// A float indicating the current Cpu usage.
+        /// </returns>
+        public static float GetCpuUsage()
+        {
+            float cpuUsage = 0;
+            PerformanceCounter? cpuCounter = null;
+      
+            try
+            {
+                cpuCounter = new("Processor", "% Processor Time", "_Total");
+                cpuUsage = GetReading(cpuCounter, TIME);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                cpuCounter?.Dispose();
+            }
+
+            return cpuUsage;
+        }
+    }
+}
