88a3aaa8764185edfdef28a8cf22c0001fb9526f
Joshuua Jiang <jjiang@matrox.com>
2024-01-23 15:06:46 -0500

    code cleanup gpu

--- CROSS_PLATFORM/CpuAndGpuMetrics/CpuAndGpuMetrics/GpuMetricRetriever.cs
+++ CROSS_PLATFORM/CpuAndGpuMetrics/CpuAndGpuMetrics/GpuMetricRetriever.cs
@@ -79,13 +79,13 @@ namespace CpuAndGpuMetrics
                     return new float[] { -99f };
                 }
 
-                // float[] totalValues = new float[instanceNames.Length];
-                float[] decodeValues = new float[instanceNames.Length];
-                float[] d3Values = new float[instanceNames.Length];
-                float[] copyValues = new float[instanceNames.Length];
-                float[] encodeValues = new float[instanceNames.Length];
-
-                string[] engtypeValues = ["engtype_3D", "engtype_VideoDecode", "engtype_Copy", "engtype_VideoEncode"];
+                Dictionary<string, float> engtypeValues = new Dictionary<string, float>
+                {
+                    { "engtype_3D", 0 },
+                    { "engtype_VideoDecode", 0 },
+                    { "engtype_Copy", 0 },
+                    { "engtype_VideoEncode", 0 }
+                };
 
                 // Loop through all instances and populate values
                 for (int i = 0; i < instanceNames.Length; i++)
@@ -95,44 +95,20 @@ namespace CpuAndGpuMetrics
                     PerformanceCounter counter = new("GPU Engine", "Utilization Percentage", instance);
 
                     float value;
-
-                    // Mapping/hash set
-                    if (instance.Contains("engtype_3D"))
+                    foreach (var engtype in engtypeValues.Keys)
                     {
-                        value = GetReading(counter, TIME);
-                        d3Values[i] = value;
+                        if (instance.Contains(engtype))
+                        {
+                            value = GetReading(counter, TIME);
+                            engtypeValues[engtype] += value;
+                        }
                     }
-
-                    if (instance.Contains("engtype_VideoDecode"))
-                    {
-                        value = GetReading(counter, TIME);
-                        decodeValues[i] = value;
-                    }
-
-                    if (instance.Contains("engtype_Copy"))
-                    {
-                        value = GetReading(counter, TIME);
-                        copyValues[i] = value;
-                    }
-
-                    if (instance.Contains("engtype_VideoEncode"))
-                    {
-                        value = GetReading(counter, TIME);
-                        encodeValues[i] = value;
-                    }
-
+                   
                     counter.Dispose();
 
                 }
 
-                float d3Utilization = d3Values.Sum();
-                float decodeUtilization = decodeValues.Sum();
-                float copyUtilization = copyValues.Sum();
-                float encodeUtilization = encodeValues.Sum();
-                //Console.WriteLine($"3d: {d3Utilization}, decode: {decodeUtilization}, copy: {copyUtilization}",
-                //    d3Utilization, decodeUtilization, copyUtilization);
-
-                return new float[] { d3Utilization, copyUtilization, decodeUtilization, encodeUtilization };
+                return engtypeValues.Values.ToArray();
             }
             catch (Exception ex)
             {
